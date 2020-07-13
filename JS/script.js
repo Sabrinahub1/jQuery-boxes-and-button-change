88b74d4efab900a6538b9860f5766e7dcd81d864
Sabrinahub1 <Sabrina.washington43@yahoo.com>
2020-07-13 13:33:52 -0400

    HW V2

---
+++ JS/script.js
@@ -0,0 +1,19 @@
+$(".changeBtn1").click(fucntion)(){}
+console.log ("click is working Btn 1");
+
+$(".firstDiv").addClass("firstDivNew");
+
+$(".changeBtn2").click(fucntion)(){}
+console.log ("click is working Btn2");
+$(".secondDiv").css("background )
+
+$(".changeBtn3").click(fucntion)(){}
+console.log ("click is working Btn 3");
+
+$(".changeAllBtn").click(fucntion){
+console.log ("click is working Change All");
+
+$("firstDiv").addClass("changeAllColors");
+$("firstDiv").addClass("firstDivNew");
+$("firstDiv").css("background","orange");
+$("firstDiv").css("background","yellow");
